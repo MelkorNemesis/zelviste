54e04ba5b26366f054262fa4dccc169235c04a59
Michal Ševčík <sevcik.mi@gmail.com>
2019-05-16 18:18:40 +0200

    - uprava skriptu na tvorbu admin uzivatele

--- packages/core/cli/addAdminUsers.js
+++ packages/core/cli/addAdminUsers.js
@@ -1,18 +1,32 @@
-import yargs from 'yargs'
+import program from "commander";
 
 import { run } from "./helpers";
 import { sha512, getRandomString } from "../src/server/utils";
 import { AdminUser } from "../src/server/model";
 
-run(() => {
-  const salt = getRandomString();
-  const hash = sha512({ password: "Melkor1990", salt });
+program.option("--email <email>", "User email");
+program.option("--password <password>", "User password");
+program.option("--firstname <firstname>", "User firstname");
+program.option("--surname <surname>", "User surname");
+program.parse(process.argv);
 
-  return AdminUser.query().insert({
-    firstname: "Michal",
-    surname: "Ševčík",
-    email: "sevcik.mi@gmail.com",
+const { email, password, firstname, surname } = program;
+if (!email || !password || !firstname || !surname) {
+  console.error("Missing arguments");
+  process.exit();
+}
+
+run(async () => {
+  const salt = getRandomString();
+  const hash = sha512({ password, salt });
+
+  await AdminUser.query().insert({
+    firstname,
+    surname,
+    email,
     salt,
     hash
   });
+
+  console.info(`Successfully created user <${email}>`);
 });
